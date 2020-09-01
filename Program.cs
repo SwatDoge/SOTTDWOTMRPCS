ac52c8400ba275275beee788ac6c7770ae47e5ce
SwatDoge <bob.dedebouwer@hotmail.com>
2020-09-01 17:45:45 +0200

    Start.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Topshelf;
+
+//Created using Topshelf documentation: https://dotnetcoretutorials.com/2019/09/27/creating-windows-services-in-net-core-part-2-the-topshelf-way/
+namespace DiscordWOTM {
+    class Program {
+        static void Main(string[] args) {
+            var exitCode = HostFactory.Run(x =>{
+                x.Service<RPCService>(s =>{ //Manage when services run.
+                    s.ConstructUsing(service => new RPCService());
+                    s.WhenStarted(service => service.Start(args));
+                    s.WhenStopped(service => service.Stop());
+                });
+                //Set service settings.
+                x.RunAsLocalSystem();
+
+                x.SetDisplayName("Swats WOTM discord RPC Service");
+                x.SetServiceName("SwatsWOTMDiscordRPC");
+                x.SetDescription("This service will update a discord status at any time.");
+                x.StartAutomaticallyDelayed();
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
---
+++ RPCService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using DiscordRPC;
+using System.Timers;
+using System.Diagnostics;
+
+namespace DiscordWOTM {
+    public class RPCService {
+
+        private string random_word = "";
+        private static int discord_pipe = -1;
+        private readonly Timer update_cooldown;
+        private readonly DiscordRpcClient client;
+
+        public RPCService() { //Setup one time requirements.
+            update_cooldown = new Timer(60000) { AutoReset = true };
+            update_cooldown.Elapsed += update_presence;
+            client = new DiscordRpcClient("747167926242640003", pipe: discord_pipe) {/*Logger = new DiscordRPC.Logging.ConsoleLogger(DiscordRPC.Logging.LogLevel.Info, true)*/};
+        }
+
+        private static string get_random_word() { //Get a random word thru random-word-api
+            System.Net.WebClient wc = new System.Net.WebClient();
+            return wc.DownloadString("https://random-word-api.herokuapp.com/word?number=1&swear=0").Trim('[', ']', '\"');
+        }
+
+        private void update_presence(Object source, ElapsedEventArgs e) { //Updates presence every 60 seconds.
+            if (has_connection_to_internet() && has_discord_running()) {
+                random_word = get_random_word();
+                client.SetPresence(new RichPresence() { State = random_word });
+            }
+        }
+
+        private static bool has_discord_running() { //Checks if the discord executable is running.
+            if (Process.GetProcessesByName("discord").Length > 0) return true;
+            return false;
+        }
+
+        private static bool has_connection_to_internet() { //Checks if discord is online.
+            try {
+                using (var wc = new System.Net.WebClient())
+                using (wc.OpenRead("https://discord.com/"))
+                return true;
+            }
+            catch {
+                return false;
+            }
+        }
+
+        public void Start(string[] args) { //Start function when service starts.
+            if (has_connection_to_internet() && has_discord_running()) {
+                for (int i = 0; i < args.Length; i++) { //Set pipeline
+                    switch (args[i]) {
+                        case "-pipe":
+                            discord_pipe = int.Parse(args[++i]);
+                            break;
+                        default: break;
+                    }
+                }
+                random_word = get_random_word();
+                client.SetPresence(new RichPresence() { State = random_word });
+                update_cooldown.Start();
+                client.Initialize();
+            }
+        }
+
+        public void Stop() { //Stops and disposes on server close.
+            update_cooldown.Stop();
+            client.Dispose();
+        }
+    }
+}
