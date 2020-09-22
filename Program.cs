f56dd8a7e2fd9782afb8e163905cd85ec97ddd77
SwatDoge <bob.dedebouwer@hotmail.com>
2020-09-22 22:29:26 +0200

    Update
    A huge update which introduces more efficiency regarding api calls and extra details in the rich presence.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using DiscordWOTM;
+using System;
 using Topshelf;
 
 //Created using Topshelf documentation: https://dotnetcoretutorials.com/2019/09/27/creating-windows-services-in-net-core-part-2-the-topshelf-way/
-namespace DiscordWOTM {
-    class Program {
+namespace DiscordWOTMRPCS {
+    static class Program {
         static void Main(string[] args) {
-            var exitCode = HostFactory.Run(x =>{
-                x.Service<RPCService>(s =>{ //Manage when services run.
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<RPCService>(s =>
+                { //Manage when services run.
                     s.ConstructUsing(service => new RPCService());
                     s.WhenStarted(service => service.Start(args));
                     s.WhenStopped(service => service.Stop());
--- RPCService.cs
+++ RPCService.cs
@@ -1,32 +1,75 @@
-﻿using System;
-using DiscordRPC;
-using System.Timers;
+﻿using DiscordRPC;
+using DiscordWOTMRPCS.Properties;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Timers;
 
 namespace DiscordWOTM {
     public class RPCService {
 
-        private string random_word = "";
         private static int discord_pipe = -1;
         private readonly Timer update_cooldown;
+        private readonly Timer update_totaltime;
         private readonly DiscordRpcClient client;
+        private static List<string> word_list = JsonConvert.DeserializeObject<List<string>>(Settings.Default.word_list);
+        static bool started = false;
 
         public RPCService() { //Setup one time requirements.
             update_cooldown = new Timer(60000) { AutoReset = true };
             update_cooldown.Elapsed += update_presence;
-            client = new DiscordRpcClient("747167926242640003", pipe: discord_pipe) {/*Logger = new DiscordRPC.Logging.ConsoleLogger(DiscordRPC.Logging.LogLevel.Info, true)*/};
+
+            update_totaltime = new Timer(1000) { AutoReset = true };
+            update_totaltime.Elapsed += appendTime;
+
+            client = new DiscordRpcClient("747167926242640003", pipe: discord_pipe) {};
+        }
+
+        void appendTime(Object source, ElapsedEventArgs e) {
+            Settings.Default.total_time = Settings.Default.total_time.Add(TimeSpan.FromSeconds(1));
+            Settings.Default.Save();
+            if (Settings.Default.total_time.Seconds == 0 && !started) {
+                update_cooldown.Start();
+                if (has_connection_to_internet() && has_discord_running()) set_presence();
+                started = true;
+            }
+        }
+
+
+        private void update_presence(Object source, ElapsedEventArgs e) {
+            if (has_connection_to_internet() && has_discord_running()) set_presence();
         }
 
         private static string get_random_word() { //Get a random word thru random-word-api
-            System.Net.WebClient wc = new System.Net.WebClient();
-            return wc.DownloadString("https://random-word-api.herokuapp.com/word?number=1&swear=0").Trim('[', ']', '\"');
+            if (word_list.Count <= 0) {
+                System.Net.WebClient wc = new System.Net.WebClient();
+                Settings.Default.word_list = wc.DownloadString("https://random-word-api.herokuapp.com/word?number=300&swear=1");
+                Settings.Default.Save();
+                word_list = JsonConvert.DeserializeObject<List<string>>(Settings.Default.word_list);
+            }
+            string word = word_list[0];
+            Settings.Default.word_list = JsonConvert.SerializeObject(word_list);
+            Settings.Default.Save();
+            word_list.RemoveAt(0);
+            return word;
         }
 
-        private void update_presence(Object source, ElapsedEventArgs e) { //Updates presence every 60 seconds.
-            if (has_connection_to_internet() && has_discord_running()) {
-                random_word = get_random_word();
-                client.SetPresence(new RichPresence() { State = random_word });
-            }
+        private long get_word_count() {
+            Settings.Default.word_count++;
+            Settings.Default.Save();
+            return Settings.Default.word_count;
+        }
+
+        private void set_presence() {
+            Timestamps timestamp = new Timestamps() { Start = DateTime.UtcNow.Subtract(Settings.Default.total_time) };
+
+            client.SetPresence(new RichPresence()
+            {
+                Details = get_random_word(),
+                State = "total: " + get_word_count().ToString(),
+                Timestamps = timestamp
+            });
         }
 
         private static bool has_discord_running() { //Checks if the discord executable is running.
@@ -38,7 +81,7 @@ namespace DiscordWOTM {
             try {
                 using (var wc = new System.Net.WebClient())
                 using (wc.OpenRead("https://discord.com/"))
-                return true;
+                    return true;
             }
             catch {
                 return false;
@@ -55,15 +98,15 @@ namespace DiscordWOTM {
                         default: break;
                     }
                 }
-                random_word = get_random_word();
-                client.SetPresence(new RichPresence() { State = random_word });
-                update_cooldown.Start();
+                set_presence();
+                update_totaltime.Start();
                 client.Initialize();
             }
         }
 
         public void Stop() { //Stops and disposes on server close.
             update_cooldown.Stop();
+            update_totaltime.Stop();
             client.Dispose();
         }
     }
